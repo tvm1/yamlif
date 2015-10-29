5ec8f9e28d25bfd1cadf950431b793f5cbb62530
Tomas Volka <tomas.volka@gmail.com>
2015-10-30 00:13:51 +0100

    Cleanup.

--- yamlif.py
+++ yamlif.py
@@ -5,13 +5,13 @@ import curses
 import yaml
 
 
-def initcurses():
+def init_curses():
     stdscr = curses.initscr()
 
     maxy, maxx = stdscr.getmaxyx()
 
     if maxy < 24 or maxx < 80:
-        cleancurses()
+        clean_curses()
         print("Sorry, Only 80x24+ consoles are supported!")
         quit(1)
 
@@ -38,10 +38,58 @@ def initcurses():
     return stdscr
 
 
-def cleancurses():
+def clean_curses():
     curses.curs_set(1)
     curses.nocbreak()
     curses.endwin()
+
+
+def draw_selector(screen, title="empty", menu=None, msel=0):
+    if not menu:
+        menu = ["empty"]
+    maxy, maxx = screen.getmaxyx()
+
+    size_y = len(menu) + 2
+    size_x = len(max(menu, key=len)) + 2
+
+    pos_y = int(maxy / 2 - size_y / 2)
+    pos_x = int(maxx / 2 - size_x / 2)
+
+    screen.addstr(0, 2, 'Welcome to YAMLIF!')
+
+    win = curses.newwin(size_y, size_x, pos_y, pos_x)
+
+    win.border()
+    win.attron(curses.A_BOLD)
+
+    win.addstr(0, int(size_x / 2 - len(title) / 2), title)
+
+    while True:
+        for i, mitem in enumerate(menu):
+            mitem = mitem.ljust(size_x - 2)
+            if msel == i:
+                win.addstr(i + 1, 1, str(mitem), curses.color_pair(1))
+            else:
+                win.addstr(i + 1, 1, str(mitem))
+
+        win.refresh()
+        ckey = screen.getch()
+
+        if ckey == curses.KEY_UP:
+            if msel == 0:
+                msel = len(menu) - 1
+            else:
+                msel += -1
+
+        if ckey == curses.KEY_DOWN:
+            if msel == len(menu) - 1:
+                msel = 0
+            else:
+                msel += 1
+
+        if ckey == curses.KEY_ENTER or ckey == 10:
+            del win
+            return msel
 
 
 def open_yaml(yfile):
@@ -50,13 +98,26 @@ def open_yaml(yfile):
         return yamlobj
 
 
-def print_structure(yamlobj, lvl=0):
+def print_structure(yamlobj, lvl=0, ):
     for obj in yamlobj['content']:
         if 'menu' in obj:
             print('|' + lvl * ' ' + '\\' + ' ' + obj["menu"])
-            print_structure(obj, lvl + 1)
+            if lvl != -1:
+                print_structure(obj, lvl + 1)
         elif 'page' in obj:
             print('|' + lvl * ' ' + '-' + ' ' + obj["page"])
+
+
+def get_menulist(yamlobj):
+    menuarr = []
+
+    for obj in yamlobj['content']:
+        if 'menu' in obj:
+            menuarr.append(obj["menu"])
+        elif 'page' in obj:
+            menuarr.append(obj["page"])
+
+    return menuarr
 
 
 def main():
@@ -65,11 +126,11 @@ def main():
         quit(1)
 
     yamlobj = open_yaml(sys.argv[1])
-    # print_structure(yamlobj, 0)
+    stdscr = init_curses()
 
-    stdscr = initcurses()
+    draw_selector(stdscr, title="Main menu", menu=get_menulist(yamlobj))
 
-    cleancurses()
+    clean_curses()
 
 
 if __name__ == '__main__':
