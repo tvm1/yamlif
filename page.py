ed558804d06d09229b1976468c6704d5964d4e0a
Tomas Volka <tomas.volka@gmail.com>
2015-11-06 18:32:13 +0100

    Example validation file.

---
+++ page.py
@@ -0,0 +1,20 @@
+# This is example file that processes some values from general_setup page
+# when user saves the page. A string can be returned back to
+
+
+def general_setup_validator(values):
+    log = "Validator: "
+
+    if values['sys_v_ipc'] is True:
+        values['sys_v_ipc'] = False
+        log += "Changed SYS_V_IPC to False. "
+
+    if int(values['kernel_log_buffer']) > 32:
+        values['kernel_log_buffer'] = 1
+        log += "Changed kernel_log_buffer to 1, because it was larger than 32. "
+
+    if not str(values['cpu_kernel_log_buffer']).isdigit():
+        values['cpu_kernel_log_buffer'] = 64
+        log += "Changed cpu_kernel_log_buffer to 64, because it was not number."
+
+    return log
