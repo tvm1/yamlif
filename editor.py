bc3a86ee076aa429b0f36a3a20eb43c04ac8703c
Tomas Volka <tomas.volka@gmail.com>
2015-11-07 12:22:45 +0100

    Make things pretty.

--- editor.py
+++ editor.py
@@ -315,11 +315,11 @@ class Editor(object):
         if len(self.title) >= self.win_size_x:
             self.title = self.title[:-3] + ".."
         if self.max_paragraphs == 1 and self.edit is True:
-            quick_help = "   F1: Help, F2/Enter/^x: Save, F3/ESC/^c: Cancel"
+            quick_help = "F1: Help, F2/Enter/^x: Save, F3/ESC/^c: Cancel"
         elif self.edit is False:
-            quick_help = "   q/F2/^c: Quit, j/k: Scroll up/down"
+            quick_help = "q/F2/^c: Quit, j/k: Scroll up/down"
         else:
-            quick_help = "   F2/^x: Save, F3/ESC/^c: Cancel"
+            quick_help = "F1: Help, F2/^x: Save, F3/ESC/^c: Cancel"
         if len(self.title) + len(quick_help) > self.win_size_x - 2:
             return self.title, quick_help[:self.win_size_x -
                                           len(self.title) - 3] + ".."
